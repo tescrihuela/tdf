3f9fd93120f0031937618e550720d8a3ecf6e458
Thomas Escrihuela <thomas.escrihuela@cerema.fr>
2023-07-07 18:30:19 +0200

    DashTools Automatic Push - 07/07/2023 @ 18:30:19

--- src/README.md
+++ src/README.md
@@ -2,5 +2,5 @@
 
 Accessible ici : https://fantasy-tdf.herokuapp.com/
 
-Se base sur https://tourdefrance2021.fr/ pour scrapper les résultats.
+Se base sur https://tourdefrance2023.fr/ pour scrapper les résultats.
 Calcule le score Fantasy : le nombre de points obtenus sur [Fantasy](https://fantasy.letour.fr/) par le coureur en fonction de son classement général, montagne, à points et jeune.
---
+++ src/app.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+
+import flask
+import dash
+import dash_table
+import dash_html_components as html
+import pandas as pd
+
+from tdf import coureurs, file
+
+
+#########
+# Serveur
+server = flask.Flask(__name__)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, server=server, title='Fantasy Cheat', external_stylesheets=external_stylesheets)
+app.config.suppress_callback_exceptions = True
+server = app.server
+
+df = pd.read_csv(file)
+
+##############
+### Layout
+app.layout = html.Div(
+    children=[
+        html.H1('Tour de France 2023 Fantasy League',className='logo'),
+        html.Div([
+            dash_table.DataTable(
+                id='datatable-interactivity',
+                columns=[
+                    {"name": i, "id": i, "deletable": False, "selectable": True} for i in df.columns
+                ],
+                # tooltip={'Fantasy Score': {'type': 'text', 'value': 'Points obtenus lors de la prochaine étape si aucun changement aux classements'}},
+                style_header={
+                    'backgroundColor': 'rgb(230, 230, 230)',
+                    'fontWeight': 'bold'
+                },
+                style_cell_conditional=[
+                    {
+                        'if': {'column_id': col},
+                        'textAlign': 'left'
+                    } for col in ['Coureur','Equipe']
+                ] + [
+                    {
+                        'if': {'column_id': col},
+                        'fontWeight': 'bold'
+                    } for col in ['Fantasy Score']
+                ],
+                style_data_conditional=[
+                    {
+                        'if': {'row_index': 'odd'},
+                        'backgroundColor': 'rgb(248, 248, 248)'
+                    }
+                ],
+                data=df.to_dict('records'),
+                # editable=True,
+                filter_action="native",
+                sort_action="native",
+                sort_mode="multi",
+                # column_selectable="single",
+                # row_selectable="multi",
+                # row_deletable=True,
+                selected_columns=[],
+                selected_rows=[],
+                page_action="native",
+                # page_current= 0,
+                page_size= 300,
+            )
+        ]),
+        html.Div(
+            children=[
+                "Source : ", html.A("Github", href="https://github.com/tescrihuela/tdf"),
+            ]
+        )
+    ]
+)
+
+
+######
+# Main
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
